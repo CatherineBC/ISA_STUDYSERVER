338facdb8e41922d628abed2c70e51bf62f44c89
CatherineBC <catherinehuangbc@gmail.com>
2023-04-11 22:55:01 +0700

    apa dah

---
+++ ProjectISA_StudyServer/ProjectISA_StudyServer/FormKonfirmasiToko.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace ProjectISA_StudyServer
+{
+    partial class FormKonfirmasiToko
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDaftarSupplier = new System.Windows.Forms.Label();
+            this.dataGridViewData = new System.Windows.Forms.DataGridView();
+            this.buttonKeluar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelDaftarSupplier
+            // 
+            this.labelDaftarSupplier.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.labelDaftarSupplier.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDaftarSupplier.ForeColor = System.Drawing.Color.LightCoral;
+            this.labelDaftarSupplier.Location = new System.Drawing.Point(0, -5);
+            this.labelDaftarSupplier.Name = "labelDaftarSupplier";
+            this.labelDaftarSupplier.Size = new System.Drawing.Size(1077, 124);
+            this.labelDaftarSupplier.TabIndex = 35;
+            this.labelDaftarSupplier.Text = "Daftar Konfirmasi Toko";
+            this.labelDaftarSupplier.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridViewData
+            // 
+            this.dataGridViewData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewData.Location = new System.Drawing.Point(8, 122);
+            this.dataGridViewData.Name = "dataGridViewData";
+            this.dataGridViewData.RowHeadersWidth = 62;
+            this.dataGridViewData.RowTemplate.Height = 28;
+            this.dataGridViewData.Size = new System.Drawing.Size(1059, 321);
+            this.dataGridViewData.TabIndex = 36;
+            // 
+            // buttonKeluar
+            // 
+            this.buttonKeluar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.buttonKeluar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKeluar.ForeColor = System.Drawing.Color.LightCoral;
+            this.buttonKeluar.Location = new System.Drawing.Point(29, 473);
+            this.buttonKeluar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonKeluar.Name = "buttonKeluar";
+            this.buttonKeluar.Size = new System.Drawing.Size(138, 44);
+            this.buttonKeluar.TabIndex = 39;
+            this.buttonKeluar.Text = "Keluar";
+            this.buttonKeluar.UseVisualStyleBackColor = false;
+            // 
+            // FormKonfirmasiToko
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 544);
+            this.Controls.Add(this.buttonKeluar);
+            this.Controls.Add(this.dataGridViewData);
+            this.Controls.Add(this.labelDaftarSupplier);
+            this.Name = "FormKonfirmasiToko";
+            this.Text = "FormKonfirmasiToko";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDaftarSupplier;
+        private System.Windows.Forms.DataGridView dataGridViewData;
+        private System.Windows.Forms.Button buttonKeluar;
+    }
+}
--- ProjectISA_StudyServer/Study_LIB/Produk_has_Penjual.cs
+++ ProjectISA_StudyServer/Study_LIB/Produk_has_Penjual.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Study_LIB
 {
-    class Produk_has_Penjual
+    public class Produk_has_Penjual
     {
     }
 }
